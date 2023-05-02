ddf11b9d03406622e5c238aa3aa56763ec09d57d
xgokhankose <xgokhankose@hotmail.com>
2023-05-02 12:22:47 +0300

    removeTransactionByDate fixed

--- src/redux/WalletReducer.tsx
+++ src/redux/WalletReducer.tsx
@@ -53,9 +53,14 @@ const walletSlice = createSlice({
             if (!transactionToRemove) {
                 return;
             }
-            state.totalAmount -= transactionToRemove.income ? transactionToRemove.amount : -transactionToRemove.amount;
-            state.totalIncome -= transactionToRemove.income ? transactionToRemove.amount : 0;
-            state.totalExpense -= transactionToRemove.income ? 0 : transactionToRemove.amount;
+
+            const initialCurrencyObj = currency.find(obj => obj.name === state.currency);
+            const incomingCurrencyObj = currency.find(obj => obj.name === transactionToRemove.currency);
+            const ratio = initialCurrencyObj && incomingCurrencyObj ? initialCurrencyObj.value / incomingCurrencyObj.value : 0
+
+            state.totalAmount -= transactionToRemove.income ? transactionToRemove.amount * ratio : -transactionToRemove.amount * ratio;
+            state.totalIncome -= transactionToRemove.income ? transactionToRemove.amount * ratio : 0;
+            state.totalExpense -= transactionToRemove.income ? 0 : transactionToRemove.amount * ratio;
             state.transactions = state.transactions.filter((t) => t.date !== action.payload);
         },
         editBalance: (state, action: PayloadAction<string>) => {
@@ -63,7 +68,6 @@ const walletSlice = createSlice({
             const initialCurrencyObj = currency.find(obj => obj.name === state.currency);
             const incomingCurrencyObj = currency.find(obj => obj.name === transaction);
             const ratio = initialCurrencyObj && incomingCurrencyObj ? initialCurrencyObj.value / incomingCurrencyObj.value : 0
-
             state.totalAmount *= ratio
             state.totalIncome *= ratio
             state.totalExpense *= ratio
@@ -71,39 +75,39 @@ const walletSlice = createSlice({
         },
         updateTransactionByDate: (state, action: PayloadAction<{ date: string; updatedTransaction: Transaction }>) => {
             const { date, updatedTransaction } = action.payload;
-          
+
             const transactionToUpdate = state.transactions.find((t) => t.date === date);
-          
+
             if (!transactionToUpdate) {
-              return;
+                return;
             }
-          
+
             transactionToUpdate.amount = updatedTransaction.amount;
             transactionToUpdate.currency = updatedTransaction.currency;
             transactionToUpdate.description = updatedTransaction.description;
             transactionToUpdate.date = updatedTransaction.date;
             transactionToUpdate.income = updatedTransaction.income;
-          
+
             const initialCurrencyObj = currency.find((obj) => obj.name === state.currency);
             const incomingCurrencyObj = currency.find((obj) => obj.name === updatedTransaction.currency);
             const ratio = initialCurrencyObj && incomingCurrencyObj ? initialCurrencyObj.value / incomingCurrencyObj.value : 0;
-          
+
             state.totalAmount =
-              state.totalAmount -
-              (transactionToUpdate.income ? transactionToUpdate.amount : -transactionToUpdate.amount) +
-              (updatedTransaction.income ? updatedTransaction.amount : -updatedTransaction.amount) * ratio;
-          
+                state.totalAmount -
+                (transactionToUpdate.income ? transactionToUpdate.amount : -transactionToUpdate.amount) +
+                (updatedTransaction.income ? updatedTransaction.amount : -updatedTransaction.amount) * ratio;
+
             state.totalIncome =
-              state.totalIncome -
-              (transactionToUpdate.income ? transactionToUpdate.amount : 0) +
-              (updatedTransaction.income ? updatedTransaction.amount : 0) * ratio;
-          
+                state.totalIncome -
+                (transactionToUpdate.income ? transactionToUpdate.amount : 0) +
+                (updatedTransaction.income ? updatedTransaction.amount : 0) * ratio;
+
             state.totalExpense =
-              state.totalExpense -
-              (transactionToUpdate.income ? 0 : transactionToUpdate.amount) +
-              (updatedTransaction.income ? 0 : updatedTransaction.amount) * ratio;
-          },
-          
+                state.totalExpense -
+                (transactionToUpdate.income ? 0 : transactionToUpdate.amount) +
+                (updatedTransaction.income ? 0 : updatedTransaction.amount) * ratio;
+        },
+
 
     },
 });
