c86f24f8464f7ff189e02547b596ea42eaa4fa3b
xgokhankose <xgokhankose@hotmail.com>
2023-05-02 13:35:00 +0300

    updateTransactionByDate fixed

--- src/redux/WalletReducer.tsx
+++ src/redux/WalletReducer.tsx
@@ -81,6 +81,12 @@ const walletSlice = createSlice({
             if (!transactionToUpdate) {
                 return;
             }
+            const initialCurrencyObj = currency.find((obj) => obj.name === state.currency);
+            const incomingCurrencyObj = currency.find((obj) => obj.name === updatedTransaction.currency);
+            const transactionCurrencyObj = currency.find((obj) => obj.name === transactionToUpdate.currency);
+
+            const oldRatio = transactionCurrencyObj && initialCurrencyObj ? initialCurrencyObj.value / transactionCurrencyObj.value : 0;
+            const newRatio = initialCurrencyObj && incomingCurrencyObj ? initialCurrencyObj.value / incomingCurrencyObj.value : 0;
 
             transactionToUpdate.amount = updatedTransaction.amount;
             transactionToUpdate.currency = updatedTransaction.currency;
@@ -88,24 +94,20 @@ const walletSlice = createSlice({
             transactionToUpdate.date = updatedTransaction.date;
             transactionToUpdate.income = updatedTransaction.income;
 
-            const initialCurrencyObj = currency.find((obj) => obj.name === state.currency);
-            const incomingCurrencyObj = currency.find((obj) => obj.name === updatedTransaction.currency);
-            const ratio = initialCurrencyObj && incomingCurrencyObj ? initialCurrencyObj.value / incomingCurrencyObj.value : 0;
-
-            state.totalAmount =
-                state.totalAmount -
-                (transactionToUpdate.income ? transactionToUpdate.amount : -transactionToUpdate.amount) +
-                (updatedTransaction.income ? updatedTransaction.amount : -updatedTransaction.amount) * ratio;
+            state.totalAmount = state.totalAmount -
+                (transactionToUpdate.income ? transactionToUpdate.amount * oldRatio : -transactionToUpdate.amount * oldRatio) +
+                (updatedTransaction.income ? updatedTransaction.amount * newRatio : -updatedTransaction.amount * newRatio);
 
             state.totalIncome =
                 state.totalIncome -
-                (transactionToUpdate.income ? transactionToUpdate.amount : 0) +
-                (updatedTransaction.income ? updatedTransaction.amount : 0) * ratio;
+                (transactionToUpdate.income ? transactionToUpdate.amount * oldRatio : 0) +
+                (updatedTransaction.income ? updatedTransaction.amount * newRatio : 0);
 
             state.totalExpense =
                 state.totalExpense -
-                (transactionToUpdate.income ? 0 : transactionToUpdate.amount) +
-                (updatedTransaction.income ? 0 : updatedTransaction.amount) * ratio;
+                (transactionToUpdate.income ? 0 : transactionToUpdate.amount * oldRatio) +
+                (updatedTransaction.income ? 0 : updatedTransaction.amount * newRatio);
+
         },
 
 
